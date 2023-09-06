3013598374eccecf4de7e11b05024e0428d13d9a
verghil <verghil@mail.ru>
2023-09-06 14:38:14 +0300

    swicht broadcas to personal messages and added __pycashe__ ti .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
 from router import router as chat
 
 app = FastAPI(
@@ -20,13 +19,13 @@ class ConnectionManager:
         self.active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
+        await self.save_messages(message=message)
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str, add_to_json:bool):
-        if add_to_json:
-            await self.save_messages(message=message)
+    async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
+
 
     @staticmethod
     async def save_messages(message:str):
@@ -43,7 +42,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}", add_to_json=True)
+            await manager.send_personal_message(f"Client: {client_id} says: {data}", websocket)
+            # await manager.broadcast(f"Client #{client_id} says: {data}", add_to_json=True)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat", add_to_json=False)
+        # await manager.broadcast(f"Client #{client_id} left the chat")
